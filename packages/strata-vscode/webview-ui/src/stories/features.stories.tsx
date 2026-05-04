e67e575c44c0537251616d01c60192a4bcec51de
alxgvts <alxgvts@gmai.com>
2026-05-04 10:19:40 -0500

    fix style

--- packages/strata-vscode/webview-ui/src/stories/features.stories.tsx
+++ packages/strata-vscode/webview-ui/src/stories/features.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from "storybook-solidjs"
+import type { Meta, StoryObj } from "storybook-solidjs-vite"
 import FeaturesTab from "../components/settings/FeaturesTab"
 import Settings from "../components/settings/Settings"
 import { StoryProviders } from "./StoryProviders"
